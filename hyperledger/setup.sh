d8ef5d6e53a91b08144ae959ce29a4018c279dd9
Vladyslav Liakh <vladyslav.liakh@kwiziq.com>
2025-05-11 14:42:31 +0300

    Added missed bits

--- hyperledger/setup.sh
+++ hyperledger/setup.sh
@@ -1,6 +1,6 @@
 echo ################################################ EXPOSING DB ENV VARS ###############################################################
 export $(cat .env | xargs)
- 
+
 echo ################################################ Generating Crypto Materials e.x. channel.tx, genesis.block ###############################################################
 if [[ $1 == "artifact" ]]; then
     sh ./fabric_artifacts/create-artifacts.sh init #can set "cont" for updating existing crypto materials
@@ -8,6 +8,16 @@ fi
 
 echo ################################################ Wake Up Fabric Containers ###############################################################
 if [[ $1 == "u" ]]; then
+    apt update && apt install -y git git-lfs nano jq docker docker-compose gzip
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install 14.21.3
+    nvm use 14.21.3
+    nvm alias default 14.21.3
+    chmod -R +x fabric_bin/*
+    sudo cp fabric_bin/* /usr/local/bin/
+    sudo tar -C /usr/local -xzf go1.13.6.linux-amd64.tar.gz.1
     docker-compose -f docker-compose.yaml up -d
     sleep 60
     echo ################################################ Creating Channel/Joining All Peers There ###############################################################
